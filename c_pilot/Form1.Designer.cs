9d8d14d9686f864258c30d338a1a00dacf13fb5c
Alexander Mizerak <xprof81@gmail.com>
2020-04-09 22:38:03 +0300

    работает скриншот. Под Админом!!!

--- c_pilot/Form1.Designer.cs
+++ c_pilot/Form1.Designer.cs
@@ -29,12 +29,93 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Найти окно";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(401, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "No window";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Скрин";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EVE - Tapko Woman",
+            "EVE - Chu Muni"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "EVE - Tapko Woman";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- c_pilot/Form1.cs
+++ c_pilot/Form1.cs
@@ -4,17 +4,66 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BotSuite;
+using BotSuite.ImageLibrary;
 
 namespace c_pilot
 {
     public partial class Form1 : Form
     {
+        IntPtr EveWindow;
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EveWindow= Window.FindWindowByWindowTitle(comboBox1.Text);
+            if (EveWindow.ToInt64() == 0) textBox2.Text = "Error";
+            else
+            {
+                textBox2.Text = "Window ok";
+                Window.ShowWindow(EveWindow,3);
+                Window.SetFrontWindow(comboBox1.Text);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (EveWindow.ToInt64() != 0)
+            {
+
+
+                 Bitmap bmpGate = new Bitmap(Image.FromFile("gate1.bmp"));
+                 ImageData gate = new ImageData(bmpGate);
+                 Bitmap screen = ScreenShot.Create(EveWindow);
+                 screen.Save("screenshot.bmp");
+            }
+            timer1.Stop();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Window.ShowWindow(EveWindow,3);
+            Window.SetFrontWindow(comboBox1.Text);
+            timer1.Start();
         }
     }
 }
